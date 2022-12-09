df0f3aba7ac37c4b0726a0191a768cef2c8e0c6e
Paul <pangprojectlangto@gmail.com>
2022-12-10 07:40:00 +0800

    update logo

--- app/Logo.tsx
+++ app/Logo.tsx
@@ -1,13 +1,17 @@
+import logo from '/public/images/logo.png'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+
 
 
 function Logo() {
   return (
     <section className='pt-8 bg-stone-300 flex justify-center'>
         <Link href="/">
-        <Image className='pt-8 w-[200px] h-[200px] md:h-[300px] md:w-[300px] '  src='/images/logo.png ' alt='logo' width={300} height={300} />
+        <Image 
+        className='pt-8 w-[200px] h-[200px] md:h-[300px] md:w-[300px]'  
+        src={logo} alt='logo' />
         </Link>
     </section>
   )
